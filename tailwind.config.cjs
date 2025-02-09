86220d905921745a746003d1afc4e84436b21117
Chezzer <46093784+chezzzer@users.noreply.github.com>
2025-02-09 20:27:25 +1300

    actually roll back to red

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -20,7 +20,7 @@ module.exports = {
                 current: 'currentColor',
                 black: '#000000',
                 brandGrad: 'radial-gradient(109.26% 109.26% at 49.83% 13.37%, #FF343C 0%, #F06F53 100%)',
-                brand: '#eff6ff',
+                brand: '#fa4e49',
             },
             transitionDuration: {
                 250: '250ms',
